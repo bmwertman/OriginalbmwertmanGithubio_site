7f066cc56666bfc2593e7524d9f04fd745265410
bmwertman <brad.wertman@leanmeanz.com>
2014-03-24 14:37:08 -0400

    added nav button shadows

--- javascript/app.js
+++ javascript/app.js
@@ -57,7 +57,7 @@ $(document).ready(function() {
                         });
                     }
                 });
-                $( this ).dequeue();
+                $(this).dequeue();
             });
 
         //form attachment Ajax
@@ -87,29 +87,4 @@ $(document).ready(function() {
         });
     });
 });
-        // if($(window).width() > 943){
-        //     var maxHeight = 458;    // Max height for the image
-        // }
-        // // var maxWidth = 650; // Max width for the image
-
-        // var ratio = 0;  // Used for aspect ratio
-        // var width = $(this).width();    // Current image width
-        // var height = $(this).height();  // Current image height
-
-        // // Check if current height is larger than max
-        // if(height > maxHeight){
-        //     ratio = maxHeight / height; // get ratio for scaling image
-        //     $(this).css("height", maxHeight);   // Set new height
-        //     $(this).css("width", width * ratio);    // Scale width based on ratio
-        //     width = width * ratio;    // Reset width to match scaled image
-        //     height = height * ratio;    // Reset height to match scaled image
-        // }
-        // Check if the current width is larger than the max
-        // if(width > maxWidth){
-        //     ratio = maxWidth / width;   // get ratio for scaling image
-        //     $(this).css("width", maxWidth); // Set new width
-        //     $(this).css("height", height * ratio);  // Scale height based on ratio
-        //     height = height * ratio;    // Reset height to match scaled image
-        //     width = width * ratio;    // Reset width to match scaled image
-        // }
 
